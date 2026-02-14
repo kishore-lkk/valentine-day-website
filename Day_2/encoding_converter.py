0e440b6defd781ed3b29f4afa86e52d201384bfb
kishore-lkk <89062747+kishore-lkk@users.noreply.github.com>
2026-02-14 23:33:28 +0530

    Delete Day_2 directory

--- Day_2/encoding_converter.py
+++
@@ -1,4 +0,0 @@
-with open (r"Day_2\regex.py", "rt", encoding="utf-16") as content, open ("Day_2\encoding_changed.py", "wt", encoding="utf-8") as output:
-    for line in content:
-        output.writelines(line)
-    
